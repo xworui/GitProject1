c0375438bacd4218f10b673dea3f958242b67eed
gaidukov <gaidukov_nikita@mail.ru>
2022-11-05 13:48:54 +0300

    Добавили суперфункицю

---
+++ main.py
@@ -0,0 +1,15 @@
+def my_superfunction():
+    print("Много принтов богу принтов")
+
+
+def main():
+    print("Первый принт")
+    print("Второй принт")
+    print("Третий принт")
+    print("Опять в поезде")
+    my_superfunction()
+
+
+
+if __name__ == '__main__':
+    main()
